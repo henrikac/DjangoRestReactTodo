66b8a728ea3aaa9fd73725e1b066ce9ae01d5372
Henrik Christensen <sensen1695@hotmail.com>
2020-10-14 10:57:29 +0200

    added custom user model

--- api/api/api/settings.py
+++ api/api/api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Authentication
+
+AUTH_USER_MODEL = 'users.User'
+
