b7333073dfa313ad9014af9f82233c6d06cecca1
Henrik Christensen <sensen1695@hotmail.com>
2020-10-15 19:44:20 +0200

    added django-cors-headers

--- api/api/todos/models.py
+++ api/api/todos/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=50)
-    description = models.TextField()
+    description = models.TextField(default='')
     completed = models.BooleanField(default=False)
     added_at = models.DateTimeField(auto_now_add=True, editable=False)
     completed_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
-        ordering = ['-added_at']
+        ordering = ['added_at']
 
     def __str__(self):
         return self.title
--- api/api/todos/serializers.py
+++ api/api/todos/serializers.py
@@ -4,8 +4,10 @@ from .models import Todo
 
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Todo
         fields = '__all__'
-        read_only_fields = ('completed', 'added_at', 'completed_at')
+        read_only_fields = ('id', 'completed', 'added_at', 'completed_at')
 
